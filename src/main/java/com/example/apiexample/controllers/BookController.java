b7f78ea32fab2a4216cbb920c631ec9c128fad91
Marcelo Martins <marcelomartinsdev@gmail.com>
2024-03-27 17:27:28 -0300

    feat: Create Book Controller
    create the first controller, called BookController, /api/books route and the first Get request

---
+++ src/main/java/com/example/apiexample/controllers/BookController.java
@@ -0,0 +1,15 @@
+package com.example.apiexample.controllers;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/books")
+public class BookController {
+
+    @GetMapping("")
+    public String getBooks() {
+        return "oi";
+    }
+}
